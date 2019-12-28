aeb5a50854aedf2561b0f412dcb1c3314bfa028d
galihsatriawan <satriawan.galih99@gmail.com>
2019-12-28 15:55:12 +0700

    - Prepare kickstarter MVVM for network connection

---
+++ app/src/main/java/id/shobrun/stikieventorganizer/repository/NetworkBoundRepository.kt
@@ -0,0 +1,109 @@
+package id.shobrun.stikieventorganizer.repository
+
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import id.shobrun.stikieventorganizer.AppExecutors
+import id.shobrun.stikieventorganizer.api.ApiResponse
+import id.shobrun.stikieventorganizer.models.NetworkResponseModel
+import id.shobrun.stikieventorganizer.models.Resource
+
+
+abstract class NetworkBoundRepository<ResultType,
+        RequestType : NetworkResponseModel>
+@MainThread constructor(private val appExecutors: AppExecutors) {
+
+    private val result: MediatorLiveData<Resource<ResultType>> = MediatorLiveData()
+
+    init {
+
+        val loadedFromDB = this.loadFromDb()
+        result.addSource(loadedFromDB) { data ->
+            result.removeSource(loadedFromDB)
+            if (shouldFetch(data)) {
+                result.postValue(Resource.loading(null))
+                fetchFromNetwork(loadedFromDB)
+            } else {
+                result.addSource<ResultType>(loadedFromDB) { newData ->
+                    setValue(Resource.success(newData))
+                }
+            }
+        }
+    }
+
+    private fun fetchFromNetwork(loadedFromDB: LiveData<ResultType>) {
+        val apiResponse = fetchService()
+        result.addSource(apiResponse) { response ->
+            response?.let {
+                when (response.isSuccessful) {
+                    true -> {
+                        if(response.body != null){
+                            response.body?.let {
+                                appExecutors.diskIO().execute{
+                                    saveFetchData(it)
+                                    appExecutors.mainThread().execute{
+                                        // we specially request a new live data,
+                                        // otherwise we will get immediately last cached value,
+                                        // which may not be updated with latest results received from network.
+                                        val loaded = loadFromDb()
+                                        result.addSource(loaded) { newData ->
+                                            newData?.let {
+                                                setValue(Resource.success(newData))
+                                            }
+                                        }
+                                    }
+
+                                }
+
+                            }
+                        }else{
+                            appExecutors.mainThread().execute{
+                                // reload from disk whatever we had
+                                result.addSource(loadFromDb()) { newData ->
+                                    newData?.let {
+                                        setValue(Resource.success(newData))
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    false -> {
+                        result.removeSource(loadedFromDB)
+                        onFetchFailed(response.message)
+                        response.message?.let {
+                            result.addSource<ResultType>(loadedFromDB) { newData ->
+                                setValue(Resource.error(it, newData))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @MainThread
+    private fun setValue(newValue: Resource<ResultType>) {
+        result.value = newValue
+    }
+
+    fun asLiveData(): LiveData<Resource<ResultType>> {
+        return result
+    }
+
+    @WorkerThread
+    protected abstract fun saveFetchData(items: RequestType)
+
+    @MainThread
+    protected abstract fun shouldFetch(data: ResultType?): Boolean
+
+    @MainThread
+    protected abstract fun loadFromDb(): LiveData<ResultType>
+
+    @MainThread
+    protected abstract fun fetchService(): LiveData<ApiResponse<RequestType>>
+
+
+    @MainThread
+    protected abstract fun onFetchFailed(message: String?)
+}
---
+++ app/src/main/java/id/shobrun/stikieventorganizer/repository/Repository.kt
@@ -0,0 +1,3 @@
+package id.shobrun.stikieventorganizer.repository
+
+interface Repository
