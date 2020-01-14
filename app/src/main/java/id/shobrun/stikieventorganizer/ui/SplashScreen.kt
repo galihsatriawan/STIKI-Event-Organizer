377da03839b8d78de2d91f5a9cc7d2376190bd63
galihsatriawan <satriawan.galih99@gmail.com>
2020-01-14 13:24:04 +0700

    -Add Splash Screen

---
+++ app/src/main/java/id/shobrun/stikieventorganizer/ui/SplashScreen.kt
@@ -0,0 +1,49 @@
+package id.shobrun.stikieventorganizer.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+import id.shobrun.stikieventorganizer.R
+import org.jetbrains.anko.intentFor
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+    private val loadingTime = 4000L // 3 Seconds
+    private lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        progressBar = findViewById(R.id.progressBar)
+
+        Thread(Runnable {
+            doProgress()
+            goToMain()
+        }).start()
+
+    }
+    fun doProgress(){
+        var progress = 0
+        Thread.sleep(1000)
+        val max = 100
+        val cnt =90
+
+        progressBar.isIndeterminate = false
+        while (progress <= max) {
+            try {
+                Thread.sleep(loadingTime/cnt)
+                progressBar.setProgress(progress)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            progress += (max/cnt)
+
+        }
+    }
+    private fun goToMain(){
+        val mainContent = intentFor<MainActivity>()
+        startActivity(mainContent)
+        finish()
+    }
+}
